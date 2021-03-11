ae0f20197de1370fdc70a088022b21751373077f
saburou <tindalosow@gmail.com>
2021-03-11 15:19:06 +0900

    create poolstatus layout

--- app/src/main/java/com/example/myapplication1/PoolStatusFragment.java
+++ app/src/main/java/com/example/myapplication1/PoolStatusFragment.java
@@ -9,26 +9,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class PoolStatusFragment extends Fragment {
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_poolstatus, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+    @Override
+    public void onResume() {
+        super.onResume();
+        // load settings
+        // if invalid settings, display issues and do not call web api.
+        // call web api, then write response.
     }
 }
